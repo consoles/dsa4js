a9064535a8a91623a8378aa76350c83be1e3ea0c
consoles <consoles.me@gmail.com>
2020-09-08 14:37:37 +0800

    feat:leetcode46,77排列组合

--- app/leetcode/No46.permute.js
+++ app/leetcode/No46.permute.js
@@ -2,22 +2,56 @@
  * @param {number[]} nums
  * @return {number[][]}
  */
-var permute = function(nums) {
-  const res = [];
+var permute = function (nums) {
+  // 基于used数组的写法，数组下标从0开始
 
-  function backtrack(nums,track){
-    if (track.length === nums.length) {
-      res.push(track.slice);
-      return;
+  // const paths = [];
+  // const n = nums.length;
+  // const used = new Array(n).fill(false)
+  //
+  // function dfs(index, path) {
+  //   if (path.length === n) {
+  //     paths.push(path.slice())
+  //     return
+  //   }
+  //   for (let i = 0; i < nums.length; i++) {
+  //     if (!used[i]) {
+  //       path.push(nums[i])
+  //       used[i] = true
+  //       dfs(index + 1, path)
+  //       path.pop()
+  //       used[i] = false
+  //     }
+  //   }
+  // }
+  //
+  // dfs(0, [])
+  // return paths;
+
+  // 基于交换的写法
+  function swap(i, j) {
+    [nums[i], nums[j]] = [nums[j], nums[i]]
+  }
+
+  const n = nums.length
+  const paths = []
+
+  function dfs(start) {
+    if (start === n) {
+      paths.push(nums.slice())
+      return
     }
-    for (let i = 0; i < nums.length;i++) {
-      track.push(nums[i]);
-
+    for (let i = start; i < n; i++) {
+      swap(i, start)
+      dfs(start + 1)
+      swap(i, start)
     }
   }
 
-  console.log(res);
-  return res;
+  dfs(0)
+  // 基于交换的写法最后输出的结果是[3,2,1],[3,1,2]，而基于used数组的写法是顺序的
+  // console.log(paths)
+  return paths
 };
 
-permute([1,2,3]);
+permute([1, 2, 3]);
---
+++ app/leetcode/No77.combine.js
@@ -0,0 +1,92 @@
+/**
+ * @param {number} n
+ * @param {number} k
+ * @return {number[][]}
+ */
+var combine = function (n, k) {
+  // const paths = []
+  //
+  // function dfs(start, path) {
+  //   if (path.length === k) {
+  //     paths.push(path.slice())
+  //     return
+  //   }
+  //   // 遍历所有可能的搜索起点
+  //   // for (let i = start; i <= n; i++) {
+  //   //   path.push(i);
+  //   //   // 下一轮搜索，设置搜索起点要加1，因为组合数不能出现重复元素
+  //   //   dfs(i + 1, path)
+  //   //   // 重点理解：深度优先遍历有回头的过程，因此递归之前做了什么，递归之后需要做相同操作的逆向操作
+  //   //   path.pop()
+  //   // }
+  //
+  //   // 剪枝，搜索起点上界 + 接下来要选择的元素个数 - 1 = n
+  //   for (let i = start; i <= n - (k - path.length) + 1; i++) {
+  //     path.push(i)
+  //     dfs(i + 1, path)
+  //     path.pop()
+  //   }
+  // }
+  //
+  // dfs(1, [])
+  // return paths
+
+  // 思路3：每个数字可以选和不选，构建一颗二叉树
+  // const paths = []
+  //
+  // /**
+  //  *
+  //  * @param start
+  //  * @param k 剩下应该考虑的数
+  //  * @param path
+  //  */
+  // function dfs(start, k, path) {
+  //   if (k === 0) {
+  //     paths.push(path.slice())
+  //     return
+  //   }
+  //   if (start > n - k + 1) {
+  //     return
+  //   }
+  //   // if (start === n + 1) {
+  //   //   return
+  //   // }
+  //   // 不选择当前的数
+  //   dfs(start + 1, k, path)
+  //
+  //   // 选择当前数
+  //   path.push(start)
+  //   dfs(start + 1, k - 1, path)
+  //   path.pop()
+  // }
+  //
+  // dfs(1, k, [])
+  // return paths
+
+  // 思路4：排列组合公式 Cnk = Cn-1k-1 + Cn-1k
+  const paths = []
+
+  function helper(n, k, path) {
+    if (k === 0) {
+      paths.push(path.slice())
+      return
+    }
+    if (n < k) {
+      return
+    }
+    // 选n
+    path.push(n);
+    helper(n - 1, k - 1, path)
+    path.pop()
+
+    // 不选n
+    helper(n - 1, k, path)
+  }
+
+  helper(n,k,[])
+  return paths
+};
+
+const n = 4, k = 2
+
+console.log(combine(n, k))
