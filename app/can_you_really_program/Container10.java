c16ee1ec34701bd83042570c45d745653b7b8025
consoles <consoles.me@gmail.com>
2023-01-17 14:46:59 +0800

    #feat:【你真的会写代码么】浮点数精度导致的断言失败的问题

---
+++ app/can_you_really_program/Container10.java
@@ -0,0 +1,86 @@
+import java.util.HashSet;
+import java.util.Set;
+
+// 需要使用命令行参数开启断言
+// java -ea Container10.java
+public class Container10 {
+    private Set<Container10> group;
+    private double amount;
+
+    public Container10() {
+        this.group = new HashSet<>();
+        this.group.add(this);
+        this.amount = 0;
+    }
+
+    public void addWater(double amount) {
+        double amountPerContainer = amount / this.group.size();
+        if (this.amount + amountPerContainer < 0) {
+            throw new IllegalArgumentException("Not enough water to match the addWater request.");
+        }
+        double oldTotal = 0; // 保存后置条件数据
+        assert (oldTotal = groupAmount()) >= 0; // 虚拟断言
+        for (Container10 c : group) {
+            c.amount += amountPerContainer;
+        }
+        assert postAddWater(oldTotal, amount) : "addWater failed its postcondition!";
+    }
+
+    public void connectTo(Container10 other) {
+        if (group == other.group) return;
+        int size1 = group.size(),
+            size2 = other.group.size();
+        double to1 = amount * size1,
+            to2 = other.amount * size2;
+        double newAmount = (to1 + to2) / (size1 + size2);
+        for (Container10 c : other.group) {
+            group.add(c); // idea 报错：Cannot access Container10
+        }
+        for (Container10 c : other.group) {
+            c.group = group;
+        }
+        for (Container10 c : group) {
+            c.amount = newAmount;
+        }
+    }
+
+    /**
+     * 容器组中的总水量
+     *
+     * @return
+     */
+    private double groupAmount() {
+        double total = 0;
+        for (Container10 c : group) {
+            total += c.amount;
+        }
+        return total;
+    }
+
+    private boolean postAddWater(double oldTotal, double addedAmount) {
+        return isGroupBalanced() &&
+            groupAmount() == oldTotal + addedAmount; // 双精度浮点数的精确比较
+    }
+
+    /**
+     * 检查组中所有容器的水量相同
+     *
+     * @return
+     */
+    private boolean isGroupBalanced() {
+        for (Container10 c : group) {
+            if (c.amount != amount) return false;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        Container10 a = new Container10(),
+            b = new Container10(),
+            c = new Container10();
+        a.connectTo(b);
+        b.connectTo(c);
+        a.addWater(0.9);
+        System.out.println(0.3 + 0.3 + 0.3); // 0.8999999999999999
+    }
+}
---
+++ app/can_you_really_program/Container8.java
@@ -0,0 +1,97 @@
+import java.util.Arrays;
+
+// Memory3：摒弃 OOP 设计思想
+// 不为每个容器专门设置一个对象，而是让客户端使用一个整型的 id 来标识每个容器
+public class Container8 {
+    private static int group[] = new int[0]; // 容器 ID 映射到它的组, [1, 1] 表示 id0, id1 的两个容器相连接
+    private static float amount[] = new float[0]; // 从组 id 映射到容器水量
+
+    /**
+     * 工厂方法，返回容器 id
+     * @return
+     */
+    public static int newContainer() {
+        int nContainers = group.length,
+            nGroups = amount.length;
+        amount = Arrays.copyOf(amount, nGroups + 1); // 在 amount 后面补 0
+        group = Arrays.copyOf(group, nContainers + 1);
+        group[nContainers] = nGroups; // 设置新容器的组 id
+        return nContainers;
+    }
+
+    public static float getAmount(int containerId) {
+        int groupId = group[containerId];
+        return amount[groupId];
+    }
+
+    public static void addWater(int containerId, float amount) {
+        int groupId = group[containerId],
+            groupSize = groupSize(groupId);
+        Container8.amount[groupId] += amount / groupSize;
+    }
+
+    public static void connectTo(int containerId1, int containerId2) {
+        int groupId1 = group[containerId1],
+            groupId2 = group[containerId2],
+            size1 = groupSize(groupId1),
+            size2 = groupSize(groupId2);
+
+        if (groupId1 == groupId2) return;
+
+        float amount1 = amount[groupId1] * size1,
+            amount2 = amount[groupId2] * size2;
+
+        amount[groupId1] = (amount1 + amount2) / (size1 + size2);
+
+        for (int i = 0; i < group.length; i++) {
+            if (group[i] == groupId2) {
+                group[i] = groupId1;
+            }
+        }
+        // 没有任何容器属于 groupId2 了
+        removeGroupAndDefrag(groupId2);
+    }
+
+    /**
+     * defrag 即碎片整理(defragmenttation),指的是文件系统维护操作,用来确保文件占用连续的空间
+     *
+     * @param groupId
+     */
+    private static void removeGroupAndDefrag(int groupId) {
+        for (int containerId = 0; containerId < group.length; containerId++) {
+            // 将 groupId 分配给之前与组 n-1 相关联的所有容器
+            if (group[containerId] == amount.length - 1) {
+                group[containerId] = groupId;
+            }
+        }
+        amount[groupId] = amount[amount.length - 1]; // 将旧组 n-1 的 amount 复制到组 groupId
+        amount = Arrays.copyOf(amount, amount.length - 1); // 舍弃最后一个单元格，删除组 n-1，组 id 的范围变成了 [0,n-2]
+    }
+
+    private static int groupSize(int groupId) {
+//    int size = 0;
+//    for (int otherGroupId : group) {
+//      if (otherGroupId == groupId) {
+//        size++;
+//      }
+//    }
+//    return size;
+
+//    可以使用 java8 的 stream 库简化代码
+        return (int) Arrays.stream(group).filter(otherGroupId -> otherGroupId == groupId).count();
+    }
+
+  public static void main(String[] args) {
+    int a = Container8.newContainer(),
+      b = Container8.newContainer(),
+      c = Container8.newContainer(),
+      d = Container8.newContainer();
+
+      Container8.addWater(a, 12);
+      Container8.addWater(d, 8);
+      Container8.connectTo(a, b);
+
+    // a: 6.000000, b: 6.000000, c: 0.000000, d: 8.000000
+    System.out.println(String.format("a: %f, b: %f, c: %f, d: %f", Container8.getAmount(a), Container8.getAmount(b), Container8.getAmount(c), Container8.getAmount(d)));
+  }
+}
