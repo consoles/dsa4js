ced5e21afc8bede2f16f5c5d1e07f1f5f368c33d
consoles <consoles.me@gmail.com>
2025-03-13 00:14:13 +0800

    feat:leetcode#202. 快乐数-快慢指针解法

--- app/leetcode/tree/No202.happy-number.js
+++ app/leetcode/tree/No202.happy-number.js
@@ -23,5 +23,55 @@ var isHappy = function (n) {
   return false;
 };
 
+function* genNextSquareSum(n) {
+  while(true) {
+    let sum = 0;
+    while(n !==0) {
+      sum += (n % 10) ** 2
+      n = n / 10 | 0
+    }
+    yield sum
+    n = sum
+  }
+}
+
+function isHappy2(n) {
+  const set = new Set();
+  for (const n of genNextSquareSum(n)) {
+    if (n === 1) {
+      return true
+    }
+    if (set.has(n)) {
+      return false
+    }
+    set.add(n)
+  }
+  return false
+}
+
+function squareSum(n) {
+  let sum = 0;
+  while(n !== 0) {
+    sum += (n % 10) ** 2
+    n = n / 10 | 0
+  }
+  return sum
+}
+
+function isHappy3(n) {
+  // 使用 set 保存已经出现过的平方和可能会导致循环特别长的问题。如果出现循环则是一个隐式的环形链表。
+  // 可以用快慢指针来判断是否出现循环
+  let fast = n
+  let slow = n
+  while(true) {
+    fast = squareSum(fast)
+    fast = squareSum(fast)
+    slow = squareSum(slow)
+    if (fast === slow) {
+      return fast === 1
+    }
+  }
+}
+
 let ret  = isHappy(3);
 console.log(ret)
