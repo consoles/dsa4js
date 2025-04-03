6b27ba88504ba0bbd89ed5dc49ffd1b7635c66b6
consoles <consoles.me@gmail.com>
2025-04-04 01:48:46 +0800

    feat:leetcode#108. 将有序数组转换为二叉搜索树

---
+++ app/leetcode/No108.sortedArrayToBST.ts
@@ -0,0 +1,28 @@
+class TreeNode {
+    val: number
+    left: TreeNode | null
+    right: TreeNode | null
+    constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+        this.val = (val === undefined ? 0 : val)
+        this.left = (left === undefined ? null : left)
+        this.right = (right === undefined ? null : right)
+    }
+}
+
+
+function sortedArrayToBST(nums: number[]): TreeNode | null {
+    if (nums.length === 0) {
+        return null
+    }
+    const middleIndex = Math.floor(nums.length / 2)
+    const root = new TreeNode(nums[middleIndex])
+    root.left = sortedArrayToBST(nums.slice(0, middleIndex))
+    root.right = sortedArrayToBST(nums.slice(middleIndex + 1))
+    return root
+};
+
+function sortedArrayToBST2(nums: number[]): TreeNode | null {
+    function buildTree(left: number, right: number) :TreeNode |null{
+        
+    }
+};
